749076327d46ec7a6ab814d8240b4a5a95915cb7
Michael Gieser <mgieser90@gmail.com>
2024-03-03 08:18:04 -0800

    update

--- script.js
+++ script.js
@@ -1,21 +1,8 @@
 window.addEventListener('scroll', function () {
-  var scrollTrigger = document.getElementById('scroll-trigger');
-  var scrollThreshold = 740;
+  const scrollTrigger = document.getElementById('scroll-trigger');
+  const scrollThreshold = 640;
 
-  var scrollPosition = window.scrollY;
-
-  if (scrollPosition > scrollThreshold && !scrollTrigger.checked) {
-    scrollTrigger.checked = true;
-  } else if (scrollPosition <= scrollThreshold && scrollTrigger.checked) {
-    scrollTrigger.checked = false;
-  }
-});
-
-window.addEventListener('scroll', function () {
-  var scrollTrigger = document.getElementById('scroll-trigger2');
-  var scrollThreshold = 1031;
-
-  var scrollPosition = window.scrollY;
+  const scrollPosition = window.scrollY;
 
   if (scrollPosition > scrollThreshold && !scrollTrigger.checked) {
     scrollTrigger.checked = true;
@@ -25,14 +12,36 @@ window.addEventListener('scroll', function () {
 });
 
 window.addEventListener('scroll', function () {
-  var scrollTrigger = document.getElementById('scroll-trigger3');
-  var scrollThreshold = 1339;
+  const scrollTrigger = document.getElementById('scroll-trigger2');
+  const scrollThreshold = 931;
 
-  var scrollPosition = window.scrollY;
+  const scrollPosition = window.scrollY;
 
   if (scrollPosition > scrollThreshold && !scrollTrigger.checked) {
     scrollTrigger.checked = true;
   } else if (scrollPosition <= scrollThreshold && scrollTrigger.checked) {
     scrollTrigger.checked = false;
+  }
+});
+
+window.addEventListener('scroll', function () {
+  const scrollTrigger = document.getElementById('scroll-trigger3');
+  const scrollThreshold = 1239;
+
+  const scrollPosition = window.scrollY;
+
+  if (scrollPosition > scrollThreshold && !scrollTrigger.checked) {
+    scrollTrigger.checked = true;
+  } else if (scrollPosition <= scrollThreshold && scrollTrigger.checked) {
+    scrollTrigger.checked = false;
+  }
+});
+
+document.getElementById('resume-menu').addEventListener('click', function () {
+  const navBar = document.getElementById('click-trigger');
+  if (navBar.style.left === '-275px') {
+    navBar.style.left = '0';
+  } else {
+    navBar.style.left = '-275px';
   }
 });
