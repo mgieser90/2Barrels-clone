7fd99b38673c87f583d7f266ce58fa12a65c2db8
Michael Gieser <mgieser90@gmail.com>
2024-03-04 23:50:23 -0800

    link the two pages

--- script.js
+++ script.js
@@ -40,14 +40,37 @@ window.addEventListener('scroll', function () {
 document.addEventListener('DOMContentLoaded', function () {
   const navBar = document.getElementById('click-trigger');
   const resumeMenuButton = document.getElementById('resume-menu');
+  const mobileNavBackground = document.getElementById('mobile-nav-background');
+  const navExit = document.getElementById('mobile-nav-exit');
 
   navBar.style.left = '-275px'; // Set initial state to hidden
+  mobileNavBackground.style.display = 'none';
+  navExit.style.right = '-70px'; // Set initial state to hidden
 
   resumeMenuButton.addEventListener('click', function () {
     if (navBar.style.left === '-275px') {
       navBar.style.left = '0';
+      mobileNavBackground.style.display = 'block';
+      navExit.style.right = '10px';
     } else {
       navBar.style.left = '-275px';
+      mobileNavBackground.style.display = 'none';
+      navExit.style.right = '-70px';
     }
   });
+
+  mobileNavBackground.addEventListener('click', function () {
+    // Close the navigation when mobileNavBackground is clicked
+    navBar.style.left = '-275px';
+    mobileNavBackground.style.display = 'none';
+    navExit.style.right = '-70px';
+  });
+
+  navExit.addEventListener('click', function () {
+    // Close the navigation when navExit is clicked
+    navBar.style.left = '-275px';
+    mobileNavBackground.style.display = 'none';
+    navExit.style.right = '-70px';
+  });
 });
+
